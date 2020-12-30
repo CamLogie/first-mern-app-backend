74e86f8fc6e5db494dd7650d07dc4b5850efd974
Cameron Logie <cameronlogie24@gmail.com>
2020-12-30 14:52:22 +0000

    endpoints working as expected

--- server.js
+++ server.js
@@ -37,15 +37,16 @@ todoRoutes.route('/:id').get(function(req, res){
   });
 });
 
-todoRoutes.route('/add').get(function(req, res){
-  let todo = new Todo(req.body);
-  todo.save()
-      .then(todo => {
-        res.status(200).json({'todo': 'todo added successfully'});
-      })
-      .catch(err => {
-        res.status(400).send('adding new todo failed');
-      });
+todoRoutes.route('/add').post(function(req, res){
+    let todo = new Todo(req.body);
+    console.log(req.body)
+    todo.save()
+        .then(todo => {
+          res.status(200).json({'todo': 'todo added successfully'});
+        })
+        .catch(err => {
+          res.status(400).send('adding new todo failed');
+        });
 });
 
 todoRoutes.route('/update/:id').post(function(req, res){
