3973874a88baf10decd1937ce19f8570cb664484
Cameron Logie <cameronlogie24@gmail.com>
2020-12-30 14:21:56 +0000

    routes for getting todos, getting individual todos, posting a new todo, and updating an existing todo built

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const mongoose = require('mongoose');
 const PORT = 4000;
 const todoRoutes = express.Router();
 
+let Todo = require('./todo.model')
+
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/todos', todoRoutes);
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/todos', {
   useNewUrlParser: true});
@@ -17,10 +19,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/todos', {
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })
-
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
-});
 
 todoRoutes.route('/').get(function(req, res) {
   Todo.find(function(err, todos){
@@ -36,5 +34,40 @@ todoRoutes.route('/:id').get(function(req, res){
   let id = req.params.id;
   Todo.findById(id, function(err, todo){
     res.json(todo);
-  })
-})
+  });
+});
+
+todoRoutes.route('/add').get(function(req, res){
+  let todo = new Todo(req.body);
+  todo.save()
+      .then(todo => {
+        res.status(200).json({'todo': 'todo added successfully'});
+      })
+      .catch(err => {
+        res.status(400).send('adding new todo failed');
+      });
+});
+
+todoRoutes.route('/update/:id').post(function(req, res){
+  Todo.findById(req.params.id, function(err, todo){
+    if(!todo) 
+      res.status(404).send("data is not found");
+    else 
+      todo.todo_description = req.body.todo_description;
+      todo.todo_responsible = req.body.todo_responsible;
+      todo.todo_priority = req.body.todo_priority;
+      todo.todo_completed = req.body.todo_completed;
+      todo.save().then(todo => {
+          res.json('Todo updated!');
+    })
+    .catch(err => {
+      res.status(400).send("Update not possible");
+    });
+  });
+});
+
+app.use('/todos', todoRoutes);
+
+app.listen(PORT, function() {
+  console.log("Server is running on Port: " + PORT);
+});
